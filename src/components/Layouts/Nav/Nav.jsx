020a16dbbef8051d058fc34f52ce01e83cba9870
LiamHjalmarsson <Liam.hjalmarsson@hotmail.com>
2024-02-29 15:34:54 +0100

    Heading / banner component

--- src/components/Layouts/Nav/Nav.jsx
+++ src/components/Layouts/Nav/Nav.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import NavItem from './components/navItem';
 import { RiShoppingBagLine } from "react-icons/ri";
 import { FaUserAlt } from "react-icons/fa";
-import styles from "./Nav.module.css"
 
 const Nav = () => {
     let links = ["start", "gender", "dam", "nyhetsbrev"];
 
     return (
-        <nav className={styles.nav}>
+        <nav className="flex w-full items-center p-8 fixed z-10 bg-primary text-white bg-opacity-80">
 
             <div>
                 <h2>
@@ -16,7 +15,7 @@ const Nav = () => {
                 </h2>
             </div>
 
-            <ul className={styles.nav__ul}>
+            <ul className="flex flex-grow justify-center items-center gap-4">
                 {
                     links.map(link => (
                         <NavItem link={link} key={link} />
@@ -24,7 +23,7 @@ const Nav = () => {
                 }
             </ul>
 
-            <div className={styles.nav__iconContainer}>
+            <div className="flex gap-4">
                 <div>
                     <RiShoppingBagLine />
                 </div>
