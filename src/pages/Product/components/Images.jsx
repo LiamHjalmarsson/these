41616d5add2186748dc941c9f062cbb3d6fe4683
LiamHjalmarsson <Liam.hjalmarsson@hotmail.com>
2024-03-01 17:49:42 +0100

    Fake store api
    using fake store api for now will be updated with other api or create rest api

--- src/pages/Product/components/Images.jsx
+++ src/pages/Product/components/Images.jsx
@@ -4,7 +4,7 @@ import Image from '../../../components/Elements/Image';
 const Images = ({img}) => {
     return (
         <div className='w-full h-full p-4 flex flex-col'>
-            <Image img={img} custom="w-full h-full shadow-middle shadow-black" />
+            <Image img={img} custom="w-full h-full" />
             <div className='flex gap-4 h-40 mt-4'>
                 <Image img={img} custom="w-1/3 h-full" />
                 <Image img={img} custom="w-1/3 h-full" />
